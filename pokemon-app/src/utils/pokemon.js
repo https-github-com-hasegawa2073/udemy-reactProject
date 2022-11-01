93686bffed000199ee803310fd0ca2c11dec1403
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-01 18:36:48 +0900

    add: ポケモンのデータを画面に出力した

--- pokemon-app/src/utils/pokemon.js
+++ pokemon-app/src/utils/pokemon.js
@@ -7,3 +7,14 @@ export const getAllPokemon = (url) => {
       });
   });
 };
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resolve(data);
+      });
+  });
+};
