863aceecdfbf89dd55260eb0e8680a5f77c298a9
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-03 18:27:12 +0900

    add: Homeコンポーネントを作成した

--- blog-with-react-and-firebase/src/components/Home.js
+++ blog-with-react-and-firebase/src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの学習中です。これから頑張ってフロントエンドエンジニアになります。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Hasegawa</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
