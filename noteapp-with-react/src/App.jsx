20cc161c5203a86c460a0589a716b10ac3e58e2a
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-04 15:08:04 +0900

    add: ノートを開いたとき1番目が選択された状態

--- noteapp-with-react/src/App.jsx
+++ noteapp-with-react/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 import uuid from "react-uuid";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
   const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    // ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    // リロードしたとき一番上のノートをアクティブに
+    setActiveNote(notes[0].id);
+  }, []);
 
   const onAddNote = () => {
     console.log("新しくノートが追加されました");
